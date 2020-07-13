e5581d269cfa866e71b1b748778a31e29f8ca7ea
Jacob Zirbel <jacobzirbel@gmail.com>
2020-07-13 14:02:12 -0500

    Updates

---
+++ precache-manifest.7bc21b77a66d6fe7b969226a573a42b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c307ab016a775106fdaa2d7d1a1a438e",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "2d5ec683e5a6d07d8238",
+    "url": "/MemoryGame/static/css/main.df1b8f45.chunk.css"
+  },
+  {
+    "revision": "dbac796f19279e558ca1",
+    "url": "/MemoryGame/static/js/2.a84eab38.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MemoryGame/static/js/2.a84eab38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5ec683e5a6d07d8238",
+    "url": "/MemoryGame/static/js/main.01df44a8.chunk.js"
+  },
+  {
+    "revision": "96e4dc18fe0346eea689",
+    "url": "/MemoryGame/static/js/runtime-main.e760fd48.js"
+  }
+]);
